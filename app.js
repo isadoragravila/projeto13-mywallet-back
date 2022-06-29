84ec08742b3843ba12795e35ff1d3498292dc339
Isadora Ávila <isadorgravila@gmail.com>
2022-06-29 12:37:06 -0300

    cadastro de usuários com senha criptografada (/signup)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { MongoClient, ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 import joi from 'joi';
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 
@@ -18,7 +19,34 @@ mongoClient.connect().then(() => {
     db = mongoClient.db(process.env.DATABASE);
 });
 
+app.post('/signup', async (req, res) => {
+    //validação do formato do objeto vindo do body
+    const userSchema = joi.object({
+        name: joi.string().required(),
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    });
+    const validation = userSchema.validate(req.body);
+    if (validation.error) {
+        return res.sendStatus(422);
+    }
 
+    try {
+        //verificação se já existe alguém utilizando o email
+        const { email, password } = req.body;
+        const verifyEmail = await db.collection('users').findOne({ email });
+        if (verifyEmail) {
+            return res.sendStatus(409);
+        }
+        //criptografar a senha e enviar para o db
+        const encryptedPassword = bcrypt.hashSync(password, 10);
+        await db.collection('users').insertOne({ ...req.body, password: encryptedPassword });
+        res.sendStatus(201);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
 
 
 const PORT = process.env.PORT;
