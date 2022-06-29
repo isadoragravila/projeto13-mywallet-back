0ab49ebdb2eb39bb841822c8966a540847feb736
Isadora Ávila <isadorgravila@gmail.com>
2022-06-29 15:58:05 -0300

    post (/signin) - v2

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.post('/signin', async (req, res) => {
         if (user && bcrypt.compareSync(password, user.password)) {
             const token = uuid();
             await db.collection('sessions').insertOne({ userId: user._id, token });
-            return res.status(201).send(token);
+            return res.status(201).send({ token });
         } else {
             return res.status(401).send("E-mail ou senha incorretos!");
         }
