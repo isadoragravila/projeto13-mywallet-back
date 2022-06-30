a3689cfe1dd82a6eae93998a0e0ceacf0cf7bd08
Isadora Ávila <isadorgravila@gmail.com>
2022-06-30 12:47:18 -0300

    post (/registers) - Recebe token e body, valida e salva na coleção o registro

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import joi from 'joi';
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
+import dayjs from 'dayjs';
 
 dotenv.config();
 
@@ -95,6 +96,47 @@ app.get('/registers', async (req, res) => {
         const registers = await db.collection('transactions').find({ userId: new ObjectId(userId) }).toArray();
 
         return res.status(200).send({ name: user.name, registers });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.post('/registers', async (req, res) => {
+    //validação do formato do objeto vindo do body
+    const registerSchema = joi.object({
+        type: joi.string().valid("inflow", "outflow").required(),
+        description: joi.string().required(),
+        value: joi.number().required()
+    });
+    const validation = registerSchema.validate(req.body);
+    if (validation.error) {
+        return res.status(422).send("Todos os campos são obrigatórios!");
+    }
+
+    try {
+        //recebe o token e verifica se tem sessão e usuário relacionados
+        const { authorization } = req.headers;
+        const token = authorization?.replace('Bearer ', '');
+
+        const session = await db.collection('sessions').findOne({ token });
+        if (!session) {
+            return res.status(401).send("Houve algum problema session");
+        }
+
+        const userId = session.userId;
+        const user = await db.collection('users').findOne({ _id: new ObjectId(userId) });
+        if (!user) {
+            return res.status(401).send("Houve algum problema user");
+        }
+        //insere o registro na coleção transactions
+        const { type, description, value } = req.body;
+        const date = dayjs().format('DD/MM');
+
+        const register = { type, description, value: Number(value), date, userId };
+
+        await db.collection('transactions').insertOne(register);
+
+        return res.status(201).send("Registro criado com sucesso");
     } catch (error) {
         res.status(500).send(error);
     }
